45adbb46695fc806a8d6b084b5b00c0615a3cd04
HasnaNabhanArasy2200561 <147452238+HasnaNabhan23@users.noreply.github.com>
2024-04-22 20:57:31 +0700

    Update nomor4.java

--- nomor4.java
+++ nomor4.java
@@ -15,24 +15,24 @@ public class nomor4 {
         System.out.println("Luas alas :"+limasSegiEmpat1.luasAlas);
         System.out.println("Luas selubung limas :"+limasSegiEmpat1.luasSelubungLimas);
         System.out.println("Tinggi :"+limasSegiEmpat1.tinggi);
-        System.out.println("Tinggi :"+limasSegiEmpat1.getLuas());
-        System.out.println("Tinggi :"+limasSegiEmpat1.getVolume());
+        System.out.println("Luas :"+limasSegiEmpat1.getLuas());
+        System.out.println("Volume :"+limasSegiEmpat1.getVolume());
         System.out.println("");
 
         LimasSegiEmpat limasSegiEmpat2 = new LimasSegiEmpat(30,40,50);
         System.out.println("Luas alas :"+limasSegiEmpat2.luasAlas);
         System.out.println("Luas selubung limas :"+limasSegiEmpat2.luasSelubungLimas);
         System.out.println("Tinggi :"+limasSegiEmpat2.tinggi);
-        System.out.println("Tinggi :"+limasSegiEmpat2.getLuas());
-        System.out.println("Tinggi :"+limasSegiEmpat2.getVolume());
+        System.out.println("Luas :"+limasSegiEmpat2.getLuas());
+        System.out.println("Volume :"+limasSegiEmpat2.getVolume());
         System.out.println("");
 
         LimasSegiEmpat limasSegiEmpat3 = new LimasSegiEmpat(25,35,45);
         System.out.println("Luas alas :"+limasSegiEmpat3.luasAlas);
         System.out.println("Luas selubung limas :"+limasSegiEmpat3.luasSelubungLimas);
         System.out.println("Tinggi :"+limasSegiEmpat3.tinggi);
-        System.out.println("Tinggi :"+limasSegiEmpat3.getLuas());
-        System.out.println("Tinggi :"+limasSegiEmpat3.getVolume());
+        System.out.println("Luas :"+limasSegiEmpat3.getLuas());
+        System.out.println("Volume :"+limasSegiEmpat3.getVolume());
         System.out.println("");
 
     }
